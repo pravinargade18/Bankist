bc589bec64c565d007f52747479f3eb236855df3
pravinargade18 <pravinargade18@gmail.com>
2023-01-30 22:30:18 +0530

    added sticky navbar

--- script.js
+++ script.js
@@ -98,3 +98,25 @@ const hoverHandler = function (e) {
 
 nav.addEventListener('mouseover', hoverHandler.bind(0.5));
 nav.addEventListener('mouseout', hoverHandler.bind(1));
+
+
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
